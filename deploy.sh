d97a68a8d857d9e6280c3d4c9b3bf591d71e6cce
paulin02 <138277829+paulin02@users.noreply.github.com>
2024-03-29 17:31:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+if [[ $GIT_BRANCH == "dev" ]]; then
     echo "Building and pushing Docker image to dev repository in Docker Hub..."
         
     docker tag book:v1 paulinnancyjeniffer/dev:latest
     docker login -u $dockerhubusername -p $dockerhubpassword
     docker push paulinnancyjeniffer/dev:latest
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
+elif [[ $GIT_BRANCH == "main" ]]; then
     echo "Building and pushing Docker image to prod repository in Docker Hub..."
     
     docker tag book:v1 paulinnancyjeniffer/prod:latest
